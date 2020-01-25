e76c4c2bad75056fd20ac54be2da6e216880a1ea
kepa4us <realebal@gmail.com>
2020-01-25 20:28:02 +0300

    add general files

---
+++ test_items.py
@@ -0,0 +1,9 @@
+import time
+
+def test_language(browser):
+    browser.implicitly_wait(12)
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    time.sleep(3)
+    button = len(browser.find_elements_by_css_selector(".btn-add-to-basket"))
+    assert button > 0, 'No button'
