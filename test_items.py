54eae5bfd1ca18289db1bc9426f5784adc1e1b7f
kepa4us <realebal@gmail.com>
2020-01-25 20:31:26 +0300

    add time sleep

--- test_items.py
+++ test_items.py
@@ -4,6 +4,6 @@ def test_language(browser):
     browser.implicitly_wait(12)
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    time.sleep(3)
+    time.sleep(30)
     button = len(browser.find_elements_by_css_selector(".btn-add-to-basket"))
     assert button > 0, 'No button'
